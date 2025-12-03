4c84b2d0231707963d08289d85ea00520be3a1f1
jose <jose12087508@gmail.com>
2025-12-03 18:19:12 -0500

    update google auth options

--- components/GoogleAuthButton.tsx
+++ components/GoogleAuthButton.tsx
@@ -17,6 +17,10 @@ export default function GoogleAuthButton({ text = 'Continuar con Google' }: { te
             provider: 'google',
             options: {
                 redirectTo: `${window.location.origin}/auth/callback`,
+                queryParams: {
+                    access_type: 'offline',
+                    prompt: 'consent',
+                },
             },
         });
 
