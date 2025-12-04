4a3425753ffba24138fa906a6d84d8d0f2b43630
jose <jose12087508@gmail.com>
2025-12-03 20:28:42 -0500

    revert next config changes

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  generateBuildId: async () => {
-    return `build-${Date.now()}`
-  },
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
-          },
-        ],
-      },
-    ]
-  },
+  /* config options here */
 };
 
 export default nextConfig;
