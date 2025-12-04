c49ffcc0b281d6a44e1d8ada918d3d91dade9cf0
jose <jose12087508@gmail.com>
2025-12-03 20:23:30 -0500

    force disable caching in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  generateBuildId: async () => {
+    return `build-${Date.now()}`
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
